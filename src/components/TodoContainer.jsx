527bd4ae8f7cc72a52a76415b83e93399a5445dc
Vatsal Dave <vatsal.d@simformsolutions.com>
2023-04-24 14:31:04 +0530

    Add todo-list container

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,10 +1,12 @@
 import { Component } from "react";
 import Date from "./Date";
+import TodoList from "./TodoList";
 
 export default class TodoContainer extends Component {
   render() {
     return <div className="TodoContainer">
         <Date/>
+        <TodoList/>
         </div>;
   }
 }
