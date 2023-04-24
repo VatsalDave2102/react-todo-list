bd083e1d624c1b3d6a083435f31d03b2d046007a
Vatsal Dave <vatsal.d@simformsolutions.com>
2023-04-24 09:27:48 +0530

    Add date component

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import Date from "./Date";
+
+export default class TodoContainer extends Component {
+  render() {
+    return <div className="TodoContainer">
+        <Date/>
+        </div>;
+  }
+}
